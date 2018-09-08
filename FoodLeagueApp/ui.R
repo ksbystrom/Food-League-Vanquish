f958e0c209c770e1687b3ae274787a8174a41274
Kristen Bystrom <ksbystrom@gmail.com>
2018-09-08 10:04:27 -0700

    updating at 10:04 am

--- FoodLeagueApp/ui.R
+++ FoodLeagueApp/ui.R
@@ -1,30 +1,101 @@
+library(leaflet)
+library(shinycssloaders)
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# Choices for drop-downs
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
 
-library(shiny)
 
-shinyUI(fluidPage(
+navbarPage("Food League", id="nav",
+           
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  tabPanel("Interactive map",
+    div(class="outer",
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+      tags$head(
+        # Include our custom CSS
+        includeCSS("styles.css"),
+        includeScript("gomap.js")
+      ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+      leafletOutput("map", width="100%", height="100%"),
+
+      # Shiny versions prior to 0.11 should use class = "modal" instead.
+      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+        width = 330, height = "auto",
+
+        h2("ZIP explorer"),
+
+        selectInput("color", "Color", vars),
+        selectInput("size", "Size", vars, selected = "adultpop"),
+        conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+          # Only prompt for threshold when coloring or sizing by superzip
+          numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+        ),
+
+        withSpinner(plotOutput("histCentile", height = 200)),
+        withSpinner(plotOutput("scatterCollegeIncome", height = 250))
+      ),
+
+      tags$div(id="cite",
+        'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+      )
     )
-  )
-))
+  ),
+
+  navbarMenu("Data Analysis",
+  tabPanel("Data explorer",
+           tags$head(
+             # Include our custom CSS
+             includeCSS("styles.css"),
+             includeScript("gomap.js")
+           ),
+    fluidRow(
+      column(3,
+        selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+      ),
+      column(3,
+        conditionalPanel("input.states",
+          selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+        )
+      ),
+      column(3,
+        conditionalPanel("input.states",
+          selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+        )
+      )
+    ),
+    fluidRow(
+      column(1,
+        numericInput("minScore", "Min score", min=0, max=100, value=0)
+      ),
+      column(1,
+        numericInput("maxScore", "Max score", min=0, max=100, value=100)
+      )
+    ),
+    hr(),
+    withSpinner(DT::dataTableOutput("ziptable"))
+  ),
+  tabPanel("About the Developers",
+           tags$head(
+             # Include our custom CSS
+             includeCSS("styles.css"),
+             includeScript("gomap.js")),
+           fluidRow(
+             column(6,
+                    img(class="img-polaroid",
+                        src=paste0("vanquish-hackathon-landing.jpg"))),
+             column(6
+                    )
+             )
+           ),
+
+  conditionalPanel("false", icon("crosshair")))
+)
