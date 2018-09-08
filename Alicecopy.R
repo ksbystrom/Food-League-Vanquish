4a758728394adc4035450874404aaaee00193bc0
Alice Victoria <alicevictoria@dhcp-60-f8-1d-c2-a8-de.ubcvisitor.wifi.ubc.ca>
2018-09-08 10:02:31 -0700

    updated copy

--- Alicecopy.R
+++ Alicecopy.R
@@ -2,9 +2,15 @@
 ## Vanquish 
 ## September 7, 2018 
 
-write.csv(Traffic_20Counts_2FPERM_PEDESTRIAN_CT,"TrafficCountPedes.csv")
-write.csv(TrafficCounts_Bikes,"TrafficCountbikes.csv")
+#write.csv(Traffic_20Counts_2FPERM_PEDESTRIAN_CT,"TrafficCountPedes.csv")
+#write.csv(TrafficCounts_Bikes,"TrafficCountbikes.csv")
 
+#install.packages("qdap")
+library(readr)
+library(quanteda)
+library(corpus)
+library(tm)
+library(qdap)
 ############################# INJURY DATA ############################
 
 
@@ -17,7 +23,7 @@ x <- injdata$Modes
 x <- as.character(x)
 
 
-#adding a character split for single cells
+#adding a character split for single variables
 x[x == "Single Cyl"  ] <- "Single Cyl- 0" 
 x[x == "Single Veh"  ] <- "Single Veh- 0" 
 x[x == "Single Mot"  ] <- "Single Mot- 0" 
@@ -35,18 +41,80 @@ dd  <-  as.data.frame(matrix(unlist(listHolder),2))
 
 ############################# COLLISIONS DATA ############################
 #Reading in the collisions data 
-library(readr)
+
 Bikemapscoll <- read_csv("Bikemaps(collision).csv")
 
 #Removing the commas after the bikemaps collions section
 incident <- Bikemapscoll$incident_with
 incident<-gsub(",.*","",incident)
-incident
+Bikemapscoll$incident_with <-  incident
+
+
+### Starting the NLP for collisions ### 
+
+details <- Bikemapscoll$details
+#Seeing what gives the problems 
+num <- grep("Within", details)
+prob <- details[num]
+#getting rid of non ascii symbols
+Encoding(details) <- "latin1"
+iconv(details, "latin1", "ASCII", sub="")
+
+
+#make it lower case
+details <- tolower(details) 
+
+#getting rid of the punctation 
+details <- gsub('[[:punct:]]', '', details)
+
+
+# now I wish to create my bag of words
+details <- Corpus(VectorSource(details))
+stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
+stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
+details = stringr::str_replace_all(details, stopwords_regex, '')
+details
+
+#Finding the most frequent terms and plotting it. 
+frequent_terms <- freq_terms(details, 10)
+plot(frequent_terms)
+
 
 
 
 ############################# HAZARDS DATA ############################
 #Reading in the hazards data 
-library(readr)
+
+### Starting the NLP for hazards  ### 
+
 Bikemapshazards<- read_csv("Bikemaps(hazards).csv")
+
+detailsHaz<-  Bikemapshazards$details
+#getting rid of non ascii symbols
+Encoding(detailsHaz) <- "latin1"
+iconv(detailsHaz, "latin1", "ASCII", sub="")
+
+
+#make it lower cases
+detailsHaz <- tolower(detailsHaz) 
+
+#getting rid of the punctation 
+detailsHaz <- gsub('[[:punct:]]', '', detailsHaz)
+
+
+# now I wish to create my bag of words
+detailsHaz <- Corpus(VectorSource(detailsHaz))
+stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
+stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
+detailsHaz = stringr::str_replace_all(detailsHaz, stopwords_regex, '')
+detailsHaz
+
+#Finding the most frequent terms and plotting it. 
+frequent_terms <- freq_terms(detailsHaz, 10)
+plot(frequent_terms)
+
+
+
+
+
 
