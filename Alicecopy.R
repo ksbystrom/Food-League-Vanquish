04490468110a1d03820aa25ac1fe870540648afa
Alice Victoria <alicevictoria@dhcp-60-f8-1d-c2-a8-de.ubcvisitor.wifi.ubc.ca>
2018-09-07 23:48:37 -0700

    updated copy

--- Alicecopy.R
+++ Alicecopy.R
@@ -5,4 +5,26 @@
 write.csv(Traffic_20Counts_2FPERM_PEDESTRIAN_CT,"TrafficCountPedes.csv")
 write.csv(TrafficCounts_Bikes,"TrafficCountbikes.csv")
 
+injdata = read.csv("injuryData(2008-17).csv", header = TRUE)
+#Viewing data 
+injdata
+#looking at the what was involved for the accidents
+x <- injdata$Modes 
+x <- as.character(x)
+
+
+#adding a character split for single cells
+x[x == "Single Cyl"  ] <- "Single Cyl- 0" 
+x[x == "Single Veh"  ] <- "Single Veh- 0" 
+x[x == "Single Mot"  ] <- "Single Mot- 0" 
+x[x == "Single Veh"  ] <- "Single Veh- 0" 
+
+
+
+#Spliting the list from the modes 
+listHolder <- strsplit(as.character(x), "-")
+listHolder<- as.character(listHolder)
+
+
+dd  <-  as.data.frame(matrix(unlist(listHolder), 2))
 
